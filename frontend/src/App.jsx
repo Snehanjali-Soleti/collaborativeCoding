80bb931fb66a10b4fd51747777e865d7e5946180
Snehanjali <156570826+Snehanjali-Soleti@users.noreply.github.com>
2025-04-01 22:49:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 import { v4 as uuid } from "uuid";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://collaborativecoding.onrender.com");
 
 
 
